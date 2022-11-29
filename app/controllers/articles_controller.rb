0b64f28f9a5c10883d54d0804a7215b7f6f42c9f
Simion <simion.okola@student.moringaschool.com>
2022-11-29 08:39:05 +0300

    Completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,19 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
-
+  
   def show
+    session[:page_views] ||= 0
+    session[:page_views] +=1
     article = Article.find(params[:id])
+    if session[:page_views]<=3
     render json: article
+    else render json: {error: "Maximum pageview limit reached"},status: 401
+    end
   end
 
   private
